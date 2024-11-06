dfedfb669e6630cb2982a3259c87e27b25ac7f72
foolishflyfox <fenghuabin1992@163.com>
2024-11-07 06:21:34 +0800

    feat: 完成中间件demo

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { CatsService } from './cats/cats.service';
-import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import {
+  logger,
+  LoggerMiddleware,
+} from './common/middleware/logger.middleware';
 import { RouteInfo } from '@nestjs/common/interfaces';
 import { CatsController } from './cats/cats.controller';
 
@@ -19,6 +22,9 @@ import { CatsController } from './cats/cats.controller';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
+    // this.multiMiddle(consumer);
+  }
+  middlewareDemo(consumer: MiddlewareConsumer) {
     // 为指定路由设置中间件
     const proxy = consumer
       // apply() 方法可以采用单个中间件或多个参数来指定多个中间件
@@ -32,5 +38,17 @@ export class AppModule implements NestModule {
     // forRoutes() 方法可以接受单个字符串、多个字符串、一个 RouteInfo 对象、一个控制器类甚至多个控制器类
     // 在大多数情况下，你可能只会传递以逗号分隔的控制器列表,通过 exclude 排查某些路由
     proxy.exclude('cats/fff').forRoutes(CatsController);
+  }
+  // 函数式中间件
+  functionMiddle(consumer: MiddlewareConsumer) {
+    consumer
+      // 函数式中间件，当你的中间件不需要任何依赖时，请考虑使用更简单的函数式中间件替代方案
+      .apply(logger)
+      .forRoutes('cats');
+  }
+  // 多个中间件
+  multiMiddle(consumer: MiddlewareConsumer) {
+    // 为了绑定顺序执行的多个中间件，只需在 apply() 方法中提供一个逗号分隔符的列表
+    consumer.apply(LoggerMiddleware, logger).forRoutes('cats');
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { logger } from './common/middleware/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 全局中间件，将中间件绑定到每个已注册的路由
+  // 无法访问全局中间件中的 DI 容器。使用 app.use() 时可以使用 功能中间件
+  // 或者，你可以使用类中间件并在 AppModule（或任何其他模块）中将其与 .forRoutes('*')
+  app.use(logger);
   const port = process.env.PORT ?? 3000;
   console.log('server run on port:', port);
   await app.listen(port);
