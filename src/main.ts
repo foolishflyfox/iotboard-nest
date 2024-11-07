e3feb9bb7253550d3376f121d00b12c4a6eb0da0
foolishflyfox <fenghuabin1992@163.com>
2024-11-08 07:39:51 +0800

    feat: 通过 application.yaml 配置服务端口号

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
-import { NestFactory } from '@nestjs/core';
+import { ModuleRef, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { logger } from './common/middleware/logger.middleware';
 import * as fs from 'fs';
+import { AppConfigService } from './app-config/app-config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 直接从 ioc 容器中获取 provider
+  const appConfigService = app.get(AppConfigService);
   // 全局中间件，将中间件绑定到每个已注册的路由
   // 无法访问全局中间件中的 DI 容器。使用 app.use() 时可以使用 功能中间件
   // 或者，你可以使用类中间件并在 AppModule（或任何其他模块）中将其与 .forRoutes('*')
   app.use(logger);
   // app.useGlobalFilters(new HttpExceptionFilter());
-  const port = process.env.PORT ?? 3000;
+  const port = appConfigService.getServer().port ?? 3000;
   console.log('server run on port:', port);
   await app.listen(port);
   // fs.writeFile('aaa', 'abcd', () => {});
